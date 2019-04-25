326d6d3d78fc774a2cfccbb50d08c8d0105a66da
Antony <34961906+Antstoe@users.noreply.github.com>
2019-04-24 19:13:53 -0700

    Fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import Mission from './pages/Mission';
-import Learn from './pages/Learn';
-import Game from './pages/Game';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import Mission from "./pages/Mission";
+import Learn from "./pages/Learn";
+import Game from "./pages/Game";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+//Anto's Change
+// import jwt_decode from "jwt-decode";
+// import setAuthToken from "./utils/setAuthToken";
+// import { setCurrentUser, logoutUser } from "./actions/authActions";
+// import { clearCurrentProfile } from "./actions/profileActions";
+
+// import { Provider } from "react-redux";
+// import store from "./store";
 
 function App() {
   return (
