fab39860d58e009807780d69465959ccb9028fb9
Willa Jin <jin.willa@gmail.com>
2019-04-24 19:45:03 -0700

    fixed front end bugs

--- client/src/pages/Learn.js
+++ client/src/pages/Learn.js
@@ -3,15 +3,25 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import MainContainer from "../components/MainContainer";
 import Navbar from "../components/Navbar";
 import Background from "../images/nature.jpg";
 import Footer from "../components/Footer";
 import Jumbotron from "../components/Jumbotron";
 import MainButton from "../components/MainButton";
+import SecondButton from "../components/SecondButton";
+import Carousel from "../components/Carousel";
+import MainCard from "../components/MainCard";
+import SecondCard from "../components/SecondCard";
+import MainCardGroup from "../components/MainCardGroup";
+import Wrapper from "../components/Wrapper";
+import HeaderText from "../components/HeaderText";
+import BodyText from "../components/BodyText";
+import recycle from "../images/recycle.png";
 
-var sectionStyle = {
+var backgroundStyle = {
     width: "100%",
-    height: "1237px",
+    height: "985px",
     backgroundImage: `url(${Background})`
 };
 
--- client/src/pages/Mission.js
+++ client/src/pages/Mission.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import MainContainer from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Wrapper from "../components/Wrapper";
+import MainContainer from "../components/MainContainer";
 import Navbar from "../components/Navbar";
 import Background from "../images/nature.jpg";
 import Footer from "../components/Footer";
 import Jumbotron from "../components/Jumbotron";
 import MainButton from "../components/MainButton";
+import SecondButton from "../components/SecondButton";
+import Carousel from "../components/Carousel";
+import MainCard from "../components/MainCard";
+import SecondCard from "../components/SecondCard";
+import MainCardGroup from "../components/MainCardGroup";
+import Wrapper from "../components/Wrapper";
+import HeaderText from "../components/HeaderText";
+import BodyText from "../components/BodyText";
+import recycle from "../images/recycle.png";
+import cat from "../images/cat.png";
 
-var sectionStyle = {
+const backgroundStyle = {
     width: "100%",
     height: "985px",
     backgroundImage: `url(${Background})`
 };
 
+const colorStyle = {
+    backgroundColor: "white"
+};
 
 class Mission extends Component {
     render() {
